f27af3fd33885b4afccf7cdeb383560bab8b6fca
Jinu Joseph <jinuj@microsoft.com>
2016-07-15 15:51:44 -0700

    Inception Code for Warning Level

---
+++ src/Microsoft.VisualStudio.ProjectSystem.Managed/ProjectSystem/Properties/InterceptedProjectProperties/BuildPropertyPage/WarningLevelValueProvider.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.VisualStudio.ProjectSystem.Properties
+{
+    [ExportInterceptingPropertyValueProvider("WarningLevel")]
+    internal sealed class WarningLevelValueProvider : InterceptingPropertyValueProviderBase
+    {
+        private readonly UnconfiguredProject _unconfiguredProject;
+
+        [ImportingConstructor]
+        public WarningLevelValueProvider(UnconfiguredProject unconfiguredProject)
+        {
+            Requires.NotNull(unconfiguredProject, nameof(unconfiguredProject));
+
+            _unconfiguredProject = unconfiguredProject;
+        }
+
+        public override Task<string> OnSetPropertyValueAsync(string unevaluatedPropertyValue, IProjectProperties defaultProperties, IReadOnlyDictionary<string, string> dimensionalConditions = null)
+        {
+            int warningLevel;
+            if (Int32.TryParse(unevaluatedPropertyValue, out warningLevel))
+            {
+                if (!Enumerable.Range(0, 4).Contains(warningLevel))
+                {
+                    warningLevel = 0;
+                }
+            }
+            return Task.FromResult(warningLevel.ToString());
+        }
+    }
+}
